bcc8d5066528a96773856cbc0c81f5983c978242
Khai Do <3697686+zaro0508@users.noreply.github.com>
2023-02-03 08:07:12 -0800

    [DC-48] common VPC (#14)
    Decouple VPC creation from ECS cluster creation so that if we need to
    deploy multiple ECS clusters it can share a VPC. The benefits
    1. Saves costs due to not having to deploy multiple network components
    2. We don't need to setup and use up multiple CIDRs for the VPC

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import aws_cdk as cdk
+
+from common.vpc_stack import VpcStack
+from docker_fargate.docker_fargate_stack import DockerFargateStack
+
+app = cdk.App()
+vpc_stack = VpcStack(app)
+docker_fargate_stack = DockerFargateStack(app, vpc=vpc_stack.vpc)
+docker_fargate_stack.add_dependency(vpc_stack)
+
+app.synth()
---
+++ common/vpc_stack.py
@@ -0,0 +1,21 @@
+import config as config
+import helpers as helpers
+
+from aws_cdk import (Stack,
+    aws_ec2 as ec2,
+    Tags)
+
+from constructs import Construct
+
+STACK_ID = "dca-common"
+VPC_NAME = f'{STACK_ID}-vpc'
+
+class VpcStack(Stack):
+
+    def __init__(self, scope: Construct, **kwargs) -> None:
+        super().__init__(scope, STACK_ID, **kwargs)
+
+        self.vpc = ec2.Vpc(self, VPC_NAME, max_azs=2)
+
+        # Tag all resources in this Stack's scope with a cost center tag
+        Tags.of(scope).add(config.COST_CENTER_TAG_NAME, helpers.get_cost_center())

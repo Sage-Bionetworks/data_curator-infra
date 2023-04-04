4861bdb22f5c8f297f3eb6e43fc813786fb14bee
afwillia <anthony.williams@sagebase.org>
2023-04-04 10:59:36 -0700

    Remove autoscaling and create one instance with 512 vCPU.

--- docker_fargate/docker_fargate_stack.py
+++ docker_fargate/docker_fargate_stack.py
@@ -84,8 +84,8 @@ class DockerFargateStack(Stack):
             self,
             f'{stack_prefix}-Service',
             cluster=cluster,            # Required
-            cpu=256,                    # Default is 256
-            desired_count=3,            # Number of copies of the 'task' (i.e. the app') running behind the ALB
+            cpu=512,                    # Default is 256
+            desired_count=1,            # Number of copies of the 'task' (i.e. the app') running behind the ALB
             circuit_breaker=ecs.DeploymentCircuitBreaker(rollback=True), # Enable rollback on deployment failure
             task_image_options=task_image_options,
             memory_limit_mib=2048,      # Default is 512
@@ -100,7 +100,7 @@ class DockerFargateStack(Stack):
         # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_elasticloadbalancingv2/ApplicationTargetGroup.html#aws_cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup
         load_balanced_fargate_service.target_group.configure_health_check(interval=Duration.seconds(120), timeout=Duration.seconds(60))
 
-        if True: # enable/disable autoscaling
+        if False: # enable/disable autoscaling
             scalable_target = load_balanced_fargate_service.service.auto_scale_task_count(
                min_capacity=3, # Minimum capacity to scale to. Default: 1
                max_capacity=15 # Maximum capacity to scale to.
